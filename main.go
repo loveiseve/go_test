da383fcf3c3d46dfeeec9548ff8d31a61be30f0d
xiayi <xiayi@bytedance.com>
2018-02-02 21:47:08 +0800

    little complex
    Change-Id: I0aafbfe9d91c8baa2fb853d6427b77456373a277

--- main.go
+++ main.go
@@ -4,13 +4,18 @@ import "fmt"
 func main() {
 	a := 1
 	b := "hello, world"
+	c := make(chan int)
 
-	go func() {
+	go func(c chan int) {
 		fmt.Println("In goroutine")
 		t := "xiayi"
 		fmt.Println("name is %s", t)
-	}()
+		c <- 1
+	}(c)
 
 	fmt.Println("num is :%d", a)
 	fmt.Println("string is %s", b)
+
+	sig := <- c
+	fmt.Println("sig is :%d", sig)
 }
